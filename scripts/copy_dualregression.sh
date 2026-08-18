79f17b800efe5dd06ad0456ed5643d1e17d107a8
Danny Schuurman <Dannyschuurman2002@hotmail.com>
2026-08-18 16:51:13 +0200

    Updated dataset.tsv

--- scripts/copy_dualregression.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-REPO="/home/traaffneu/dansch/Documents/digital_rodent/digitalrodent"
-PROJECT="/project/4180000.73"
-
-DATASETS_TSV="$REPO/data/datasets.tsv"
-RESULTS_DIR="$REPO/results"
